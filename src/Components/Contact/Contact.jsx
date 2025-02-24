31b8791965fc2e131b263de27429e55ea1597288
nagaraj6618 <nagaraj516700@gmail.com>
2025-02-24 17:52:37 +0530

    Added Logic for contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,101 +1,153 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../Context/AuthContext';
 import { motion } from 'framer-motion';
 import axios from 'axios';
-import {prod_be_url} from "../../utils/config";
-import {showErrorToast, showSuccessToast} from "../ToastMessage/ToastMessageComponent"
+import { prod_be_url } from "../../utils/config";
+import { showErrorToast, showSuccessToast } from "../ToastMessage/ToastMessageComponent";
 
 const Contact = () => {
   const { isAuthenticated } = useAuth();
   const [formData, setFormData] = useState({ name: '', email: '', message: '' });
   const [loading, setLoading] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(true);
+  const [contactData, setContactData] = useState([]);
+  const [dataLoading, setDataLoading] = useState(true);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    // const userData = localStorage.getItem()
     const token = localStorage.getItem("token") || "";
-    console.log('Form submitted:', formData);
-    try{
-      const response = await axios.post(`${prod_be_url}/contact`,{
-        email : formData.email,
-        name : formData.name,
-        message : formData.message.trim()
-      },{
-        headers:{
-          Authorization:`Bearer ${token}`
+    try {
+      const response = await axios.post(`${prod_be_url}/contact`, {
+        email: formData.email,
+        name: formData.name,
+        message: formData.message.trim()
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`
         }
       });
-      if(response?.data?.success){
+      if (response?.data?.success) {
         showSuccessToast(response?.data.message);
       }
       setLoading(false);
-    }catch(error){
-      showErrorToast("An Error Occured.Please try again later.");
+    } catch (error) {
+      showErrorToast("An Error Occurred. Please try again later.");
       setLoading(false);
     }
   };
 
-  return (
-    <div className="flex flex-col justify-center items-center h-screen bg-gray-900 p-4">
-      <motion.h1 
-        className="text-white text-3xl font-bold mb-6" 
-        initial={{ opacity: 0, y: -20 }} 
-        animate={{ opacity: 1, y: 0 }} 
-        transition={{ duration: 0.5 }}
-      >
-        Contact Us
-      </motion.h1>
-      <form className="bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-md" onSubmit={handleSubmit}>
-        {
-          !isAuthenticated && (
-            <div>
-              <div className="mb-4">
-                <label className="block text-gray-300">Name :</label>
-                <input 
-                  name="name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-gray-700 text-white" 
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-300">Email :</label>
-                <input 
-                  name="email"
-                  type="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-gray-700 text-white" 
-                />
-              </div>
-            </div>
-          )
+  const getAllContactDetails = async () => {
+    setDataLoading(true);
+    try {
+      const token = localStorage.getItem("token") || "";
+      const response = await axios.get(`${prod_be_url}/contact`, {
+        headers: {
+          Authorization: `Bearer ${token}`
         }
-        <div className="mb-4">
-          <label className="block text-gray-300">Message :</label>
-          <textarea 
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-gray-700 text-white" 
-            rows="4"
-          ></textarea>
+      });
+      setIsAdmin(false);
+      if (response.data?.success) {
+        setIsAdmin(true);
+        setContactData(response.data.data);
+      }
+    } catch (error) {
+      setIsAdmin(false);
+    }
+    setDataLoading(false);
+  };
+
+  useEffect(() => {
+    getAllContactDetails();
+  }, []);
+
+  return (
+    <div>
+      {isAdmin ? (
+        <div className="p-6 bg-gradient-to-b from-gray-900 to-black min-h-screen">
+          <h2 className="text-white text-2xl font-bold mb-6 text-center">Contact Messages</h2>
+          { dataLoading ?(
+            <div className="flex justify-center items-center h-40">
+            <motion.div className="w-10 h-10 border-4 border-white border-t-transparent rounded-full animate-spin"></motion.div>
+          </div>
+          ):
+            contactData.length>0 ?
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {contactData.map((data, index) => (
+              <div key={index} className="bg-gradient-to-r from-gray-800 to-gray-700 p-6 rounded-lg shadow-lg border border-gray-600 transform transition duration-300 hover:scale-105 hover:shadow-xl">
+                <ul className="text-white break-words">
+                  <li className="mb-2 "><span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-teal-500">Name:</span> {data?.name}</li>
+                  <li className="mb-2"><span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-teal-500">Email:</span> {data?.email}</li>
+                  <li className="mb-2"><span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-teal-500">Message:</span> {data?.message}</li>
+                  <li className="text-gray-400"><span className="font-bold ">Posted at:</span> {data.time}</li>
+                </ul>
+              </div>
+            ))}
+          </div>
+          :
+          <p className="text-white text-center mt-10">No chat data found.</p>
+      }
         </div>
-        <button 
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300 flex justify-center items-center"
-          disabled={loading}
-        >
-          {loading ? <motion.div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin"></motion.div> : 'Send'}
-        </button>
-      </form>
+      ) : (
+        <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-b from-gray-900 to-black">
+          <motion.h1 
+            className="text-white text-3xl font-bold mb-6" 
+            initial={{ opacity: 0, y: -20 }} 
+            animate={{ opacity: 1, y: 0 }} 
+            transition={{ duration: 0.5 }}
+          >
+            Contact Us
+          </motion.h1>
+          <form className="bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-md border border-gray-700" onSubmit={handleSubmit}>
+            {!isAuthenticated && (
+              <div>
+                <div className="mb-4">
+                  <label className="block text-gray-300">Name :</label>
+                  <input 
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-gray-700 text-white" 
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-300">Email :</label>
+                  <input 
+                    name="email"
+                    type="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-gray-700 text-white" 
+                  />
+                </div>
+              </div>
+            )}
+            <div className="mb-4">
+              <label className="block text-gray-300">Message :</label>
+              <textarea 
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-gray-700 text-white" 
+                rows="4"
+              ></textarea>
+            </div>
+            <button 
+              type="submit"
+              className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300 flex justify-center items-center"
+              disabled={loading}
+            >
+              {loading ? <motion.div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin"></motion.div> : 'Send'}
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Contact;
